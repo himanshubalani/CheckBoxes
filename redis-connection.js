c79c74f325dcda5207b1893dd027cd1b99032df9
Himanshu Balani <85930567+himanshubalani@users.noreply.github.com>
2026-05-05 11:36:18 +0530

    Update redis config for production

--- redis-connection.js
+++ redis-connection.js
@@ -3,8 +3,8 @@ import 'dotenv/config';
 
 function createRedisConnection() {
 	return new Redis({
-        host: process.env.REDIS_HOST || '127.0.0.1',
-        port: process.env.REDIS_PORT || '6379',
+        host: process.env.REDIS_URL,
+        port: process.env.PORT || '6379',
 	});
 }
 
